2a0c88946daac8ef2ff913b86ba378bf8a367a60
Jan Borowiec <janekborowiec@gmail.com>
2021-12-26 12:29:44 +0100

    Cracking password recursively

--- password_cracker.py
+++ password_cracker.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Cracker:
     def __init__(self):
         self.password = self.input_password()
@@ -10,26 +13,33 @@ class Cracker:
             raise ValueError("We only accept lower case letters")
         return list(password)
 
-    def crack_letter(self, cracked_password: list) -> bool:
-        cracked = False
-        temp_password = cracked_password.copy()
-        for letter in range(97, 123):  # a to z in ASCII
-            temp_password.append(chr(letter))
-            if temp_password == self.password:
-                cracked_password = temp_password
-                print("PASSWORD CRACK SUCCESSFUL")
-                print(f"The password is: {''.join(cracker.password)}")
-                cracked = True
-            else:
-                del temp_password[-1]
-        return cracked
+    def bruteforce(self, pwd_len, pwd=[], depth=0):
+        depth += 1
+        for letter in range(97, 123):
+            pwd.append(chr(letter))
+            if depth != pwd_len:
+                pwd = self.bruteforce(pwd_len=pwd_len, pwd=pwd, depth=depth)
+            if pwd == self.password:
+                return pwd
+            del pwd[-1]
+        return pwd
 
     def crack_password(self):
+        pwd_len = 1
         cracked = False
-        cracked_password = []
         while not cracked:
-            temp_password = cracked_password.copy()
+            pwd = self.bruteforce(pwd_len=pwd_len)
+            if pwd == self.password:
+                print("Cracked!")
+                cracked = True
+            pwd_len += 1
+        return "".join(pwd)
 
 
 cracker = Cracker()
-cracker.crack_letter(cracked_password=[])
+start_time = time.time()
+cracked_password = cracker.crack_password()
+end_time = time.time()
+
+print(f"The cracked password is: {cracked_password}")
+print(f"Elapsed time: {end_time - start_time}")
